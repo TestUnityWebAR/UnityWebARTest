35a41fa3c08304be28e59cebb6ca9a37c534fcb5
TestUnityWebAR <unitywebar@gmail.com>
2025-05-16 14:42:43 +0800

    Test - 16052025, 1442

--- Build/BuildOutput.loader.js
+++ Build/BuildOutput.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 93581863,
+    wasmFileSize: 94148482,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
