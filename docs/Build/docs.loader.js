c434a89c9f8eaf6d684983aaf519466078e8563a
TestUnityWebAR <unitywebar@gmail.com>
2025-05-29 15:14:53 +0800

    Debug Cube follow image (8) - Build
    Redid the cs script using chatgpt

--- docs/Build/docs.loader.js
+++ docs/Build/docs.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 95350914,
+    wasmFileSize: 95349639,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
