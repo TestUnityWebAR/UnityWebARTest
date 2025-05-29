38c9b9d75d6a034480068e938a513ae1866a7765
TestUnityWebAR <unitywebar@gmail.com>
2025-05-29 10:47:27 +0800

    Cube follow image fix (2) - Build

--- docs/Build/docs.loader.js
+++ docs/Build/docs.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 95348311,
+    wasmFileSize: 95348546,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
