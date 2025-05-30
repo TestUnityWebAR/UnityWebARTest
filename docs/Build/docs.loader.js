44d1e43d51a4417fc0537a890401cacf39f09ce8
TestUnityWebAR <unitywebar@gmail.com>
2025-05-30 12:10:29 +0800

    Resize the Cube - Build

--- docs/Build/docs.loader.js
+++ docs/Build/docs.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 95357132,
+    wasmFileSize: 95357025,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
